b7e6ae29498fc788b208915a6ac24e9c7f4a42c0
Naiem Al Foysal <naiemalfoysal@Naiems-MacBook-Pro.local>
2022-02-23 13:24:37 +0600

    1st

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const env = require("./config/envConfig.js");
+const cors = require("cors");
+const connect = require("./config/db.js");
+const userRoutes = require("./routes/users/userRoutes.js");
+const app = express();
+
+//database connection call
+connect();
+app.use(cors());
+
+// add middaleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Naiem");
+});
+
+// user Routes
+app.use("/api", userRoutes);
+
+const port = env.PORT || 6000;
+
+app.listen(port, () => {
+  console.log(`Server is running on Port: ${port}`);
+});
